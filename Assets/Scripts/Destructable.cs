be477e02d93e59e4b62dd726f4646e0e83f53bc7
Jerod Merritt <jerodmerritt@gmail.com>
2022-12-06 15:16:08 -0600

    Made it so enemies that aren't on screen can't be destroyed

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
+    bool onScreen = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +14,21 @@ public class Destructable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < 17.0f)
+        {
+            onScreen = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!onScreen)
+        {
+            return;
+        }
+
         Projectile projectile = collision.GetComponent<Projectile>();
+
         if (projectile != null)
         {
             Destroy(gameObject);
